865d6631c1d39667030d64e69d13ab45d42c2af5
Groobles <am8972184@gmail.com>
2022-08-10 20:55:38 +0000

    typo

--- entity.py
+++ entity.py
@@ -59,7 +59,7 @@ class Entity:
         return clone
 
     def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
-        """Place this entitiy at a new location.  Handles moving across GameMaps."""
+        """Place this entity at a new location.  Handles moving across GameMaps."""
         self.x = x
         self.y = y
         if gamemap:
